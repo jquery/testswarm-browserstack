5c8005ec4cd323b02e9586e002148bbed546339a
Timo Tijhof <krinklemail@gmail.com>
2012-09-29 05:12:34 +0200

    Fix typo, trailing newline, + crontab example

--- run-sample.sh
+++ run-sample.sh
@@ -1,7 +1,9 @@
 # This is an example script that could be run by cron on a regular interval.
 # Be sure to not start it if it is running already (or use --run instead of --run-loop).
+# Example:
+# */5 * * * * ciuser ( ps aux | grep "testswarm-browserstack/src/cli.js" | grep -v grep || ( /path/to/testswarm-browserstack/run.sh 1>/var/log/testswarm-browserstack/run.log 2>&1 & ) ) > /dev/null
 #
 node /path/to/testswarm-browserstack/src/cli.js\
  --config "/path/to/testswarm-browserstack/config.json"\
- --run-loop\
- >> /var/log/testswarm-browserstack/run.log 2>&1;
+ --run-loop 50\
+ >> /var/log/testswarm-browserstack/cli.log 2>&1;
