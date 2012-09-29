aae97fc67d73487fc48ec4842e9e375971e67f19
Timo Tijhof <krinklemail@gmail.com>
2012-09-29 04:46:19 +0200

    run-sample.sh: Add note

--- run-sample.sh
+++ run-sample.sh
@@ -1,4 +1,5 @@
 # This is an example script that could be run by cron on a regular interval.
+# Be sure to not start it if it is running already (or use --run instead of --run-loop).
 #
 node /path/to/testswarm-browserstack/src/cli.js\
  --config "/path/to/testswarm-browserstack/config.json"\
