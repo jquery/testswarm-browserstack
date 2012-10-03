010ae47889df99f5f19fe799529e5a7d9f884525
Timo Tijhof <krinklemail@gmail.com>
2012-10-04 01:20:50 +0200

    Rename sample files for consistency.

--- sample-run.sh
+++ sample-run.sh
@@ -1,7 +1,7 @@
 # This is an example script that could be run by cron on a regular interval.
 # Be sure to not start it if it is running already (or use --run instead of --run-loop).
 # Example:
-# */5 * * * * ciuser ( ps aux | grep "testswarm-browserstack/src/cli.js" | grep -v grep || ( /path/to/testswarm-browserstack/run.sh 1>/var/log/testswarm-browserstack/run.log 2>&1 & ) ) > /dev/null
+# */5 * * * * ciuser ( ps aux | grep "testswarm-browserstack/src/cli.js" | grep -v grep || ( /path/to/testswarm-browserstack/run.sh 1>/var/log/testswarm-browserstack/run.log 2>&1 & ) ) > /dev/null 2>&1
 #
 node /path/to/testswarm-browserstack/src/cli.js\
  --config "/path/to/testswarm-browserstack/config.json"\
