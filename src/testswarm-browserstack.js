96b2526dd49bc1ac99e081bae0a04596051ed59a
John Cook <jcook793@gmail.com>
2013-03-02 13:43:17 -0500

    Add command-line support for listing all workers
    Closes #35.

--- src/testswarm-browserstack.js
+++ src/testswarm-browserstack.js
@@ -109,6 +109,14 @@ self = {
 		},
 
 		/**
+		 * Get a list of all workers
+		 * @param {Function} callback
+		 */
+		getWorkers: function (callback) {
+			self.browserstack.getClient().getWorkers(callback);
+		},
+
+		/**
 		 * @param {number} worker The worker id.
 		 * @param {Function} callback
 		 */
