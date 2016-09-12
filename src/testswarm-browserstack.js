jquery/testswarm-browserstack

commit b626290fa57a68d19b3d1563d768fae64d2e2e4c

Major rewrite (version 0.2.0)
Main script:
* Most importantly: The main run() and updateBrowsers() methods
  are completely rewritten to fix all known logic problems. Such
  as where it started the wrong browsers or didn't account for
  certain limits and race conditions, and it now has a priority
  formula, which (among other things) takes into account all
  online clients (not just the workers it started). So it starts
  needed browsers while also allowing other instances of this
  script (or other scripts or human users) to join the swarm
  and fill in the needed roles instead.

* Make browserstack client version explicit. v1 at this time,
  as v2's structure is still a bit odd. Maybe they'll change it
  (request has been made). If not, we'll do a minor refactoring
  to support the v2 flow.

* Added method to start a worker by swarm user agent id (e.g.
  "Chrome|22" as opposed to { browser: 'chrome', version: 22 } ).

Cli script:
* Now uses config.json for most options. It still has a few
  options to activate "verbose" and "dryRun".

* Added a --run-loop option that will execute "run" in a loop
  in child processes.
  Loop will not start if config is invalid. If config becomes
  invalid during the loop the child process will output an error
  message, but the loop will not stop and try again at the next
  interval.

* Actions removed/changes:
  - worker (new: get worker info)
  - spawn (new: start worker by testswarm ua id)
  - dry-run (replacing '--kill' and the old '--run')
  - getNeeded (not needed, info is now part of --verbose)

Misc:
* Various updates to map.js
* New code base passes JSHint (see .jshintrc)
* Updated dependencies in package.json to their latest versions
  and updated code where needed.

References:
* Fixes #16: "cli: Move defaults to commander declaration"
* Fixes #17: "stackLimit is enforced wrong (too low)"
* Fixes #21: "Online workers shouldn't take priority in the loop"
* Fixes #23: "Implement limit for similar workers"

src/testswarm-browserstack.js
@@ -1,0 +1,498 @@
+var async = require('async'),
+	browserstack = require('browserstack'),
+	request = require('request'),
+
+	browserMap = require('./map'),
+	util = require('./util'),
+
+	config = {
+		browserstack: {
+			user: undefined,
+			pass: undefined,
+			// Workers auto-terminate after 15 minutes
+			// if we're ready before that we'll terminate them.
+			// TODO: This is currently rather long, but we don't want to cut
+			// it off too soon if tests take long or if there is a backlog
+			// (better to use one worker for 30 minutes then to timeout and start
+			// new ones all the time). It'd be nice BrowserStack had a ping-system
+			// so we can ping to extend the timeout until it hits the max of 30
+			// or until it is terminated by this script when no longer needed.
+			workerTimeout: 900,
+			dryRun: false,
+			totalLimit: 10,
+			eqLimit: 2
+		},
+		testswarm: {
+			root: undefined,
+			runUrl: undefined
+		},
+		verbose: false
+	},
+	self,
+	bsClient,
+	workerToUaId;
+
+require('colors');
+
+
+/**
+ * Terminology:
+ *
+ * - worker
+ *   A (virtual) machine instance at BrowserStack that runs a certain
+ *   os and browser. Identified by a numerical ID. Every new a new VM
+ *   is spawned, it gets a new unique ID (usually higer than the
+ *   previous one).
+ *
+ * - browser
+ *   Description of a worker. To spawn a worker at BrowserStack, the
+ *   BrowserStack client is given a browser descriptor. Based on that
+ *   a VM is spawned on-demand.
+ *
+ * - ua
+ *   User-Agent identifier (string) as known to TestSwarm.
+ *   Is not specific to an actual computer.
+ *   ./map.js contains a map between `ua` and `browser`.
+ */
+
+self = {
+	/**
+	 * Set configuration variables
+	 * @param {Object} options Overrides keys in config with given values.
+	 */
+	extendConfig: function (options) {
+		if (options) {
+			util.extendObject(config, options, /* deep = */ true);
+		}
+	},
+
+	/**
+	 * Get configuration object (by reference)
+	 * @return {Object}
+	 */
+	getConfig: function () {
+		return config;
+	},
+
+	/**
+	 * Create/get the singleton Client instance for BrowserStack.
+	 * return {Client}
+	 */
+	getBsClient: function () {
+		// Lazy init
+		if (!bsClient) {
+			bsClient = browserstack.createClient({
+				version: 1,
+				username: config.browserstack.user,
+				password: config.browserstack.pass
+			});
+		}
+		return bsClient;
+	},
+
+	/**
+	 * Get the swarmstate of testswarm (number of active clients and pending runs).
+	 * @param {Function} callback
+	 */
+	getSwarmState: function (callback) {
+		request.get(config.testswarm.root + '/api.php?action=swarmstate', function (err, res, body) {
+			var apiData;
+			if (err) {
+				callback({
+					code: 'node-request',
+					info: err
+				});
+			}
+			apiData = JSON.parse(body);
+			if (apiData) {
+				if (apiData.error) {
+					callback(apiData.error);
+					return;
+				}
+				if (apiData.swarmstate) {
+					callback(null, apiData.swarmstate);
+					return;
+				}
+			}
+			callback({
+				code: 'testswarm-response',
+				info: 'Invalid API response'
+			});
+		});
+	},
+
+	/**
+	 * @param {Object} worker From Client.getWorkers.
+	 * @return {string|undefined} The uaID or undefined.
+	 */
+	getUaIdFromWorker: function (worker) {
+		var key;
+		if (!workerToUaId) {
+			// Lazy-init
+			workerToUaId = util.generateReverseMap(browserMap);
+		}
+
+		key = JSON.stringify(worker.browser);
+
+		return workerToUaId[key];
+	},
+
+	/**
+	 * Spawn a new BrowserStack worker.
+	 * @param {Object} browser
+	 */
+	spawnWorker: function (browser) {
+		if (config.browserstack.dryRun) {
+			console.log('[spawnWorker] Dry run:'.cyan, browser);
+			return;
+		}
+		var client = self.getBsClient();
+		client.createWorker({
+			browser: browser.name,
+			version: browser.version,
+			url: config.testswarm.runUrl,
+			timeout: config.browserstack.workerTimeout
+		}, function (err, worker) {
+			if (err) {
+				console.error('[spawnWorker] Error:'.red + ' Browser', browser, err);
+				return;
+			}
+			console.log('[spawnWorker]'.green, browser, worker);
+		});
+	},
+
+	/**
+	 * Spawn a new BrowserStack worker, by uaId.
+	 * @param {string} ua
+	 */
+	spawnWorkerByUa: function (ua) {
+		if (browserMap[ua]) {
+			self.spawnWorker(browserMap[ua]);
+		} else {
+			console.error('[spawnWorkerByUa] Error:'.red + ' Unknown uaId: ' + ua);
+		}
+	},
+
+	/**
+	 * @param {number} worker The worker id.
+	 */
+	terminateWorker: function (worker) {
+		if (config.browserstack.dryRun) {
+			console.log('[terminateWorker] Dry run:'.cyan + ' Terminate #' + worker);
+			return;
+		}
+		var client = self.getBsClient();
+		client.terminateWorker(worker, function (err) {
+			if (err) {
+				console.error('[terminateWorker] Error:'.red + ' Worker #' + worker + '\n', err);
+				return;
+			}
+			console.log('[terminateWorker]'.yellow + ' Terminated worker #' + worker);
+		});
+	},
+
+	/**
+	 * Main process of the library. This is where we process the swarmstate
+	 * and create and terminate workers accordingly.
+	 * @param liveWorkers Array: List of workers
+	 * @param liveSwarmState Object: Info about current state of the testswarm
+	 */
+	updateBrowsers: function (liveWorkers, liveSwarmState) {
+		var
+			uaId,
+			ua,
+			workerId,
+			worker,
+			stats,
+			result,
+
+			/**
+			 * @var {Object}
+			 * Keyed by ID, contains:
+			 *  - status (queue, running or terminated)
+			 *  - browser
+			 */
+			percWorkers,
+
+			/**
+			 * @var {Object}
+			 * Keyed by uaId, contains the numer of matching workers.
+			 */
+			workersByUa,
+
+			/**
+			 * @var {Object}
+			 * Keyed by uaId, contains:
+			 *  onlineClients, activeRuns, pendingRuns.
+			 */
+			percSwarmStats;
+
+		// Because the creation and termination is done asynchronous, and we
+		// want to save http reqests, we are going to make a copy of the live
+		// statuses of browserstack and testswarm. This copy is called the
+		// "perception", which we'll update based on what we expect/predict the
+		// status will be (e.g. we call spawnWorker and then change the status
+		// in percWorkers to 'queued').
+
+		// Task 0: Initialize perception.
+		// Also, simplify our data and make it easier to access.
+		if (config.verbose) {
+			console.log('\n== Task 0 ==\n'.white.bold);
+		}
+
+		percSwarmStats = {};
+		percWorkers = {};
+		workersByUa = {};
+
+		for (ua in liveSwarmState.userAgents) {
+			if (browserMap[ua]) {
+				percSwarmStats[ua] = util.copy(liveSwarmState.userAgents[ua].stats);
+				workersByUa[ua] = 0;
+			}
+		}
+
+		liveWorkers.forEach(function (worker) {
+			percWorkers[worker.id] = {
+				status: worker.status,
+				browser: worker.browser
+			};
+
+			uaId = self.getUaIdFromWorker(worker);
+			if (uaId) {
+				workersByUa[uaId] += 1;
+			}
+		});
+
+		console.log('Summary:', (function () {
+			var ua, summary = {};
+			for (ua in workersByUa) {
+				if (workersByUa[ua]) {
+					summary[ua] = workersByUa[ua];
+				}
+			}
+			return summary;
+		}()));
+		console.log('Live workers:\n', percWorkers);
+		if (config.verbose) {
+			console.log('Live swarm state:\n', percSwarmStats, '\n');
+
+			console.log('\n== Task 1 ==\n'.white.bold);
+		}
+
+		// Task 1: Terminate no longer needed workers
+		// - Workers for browers that have no pending and no active runs
+		//   (a job can have 0 pending runs, which means nothing is
+		//   waiting for a cliient, but runs that were already distributed may not be
+		//   finished yet. Those currently being ran by cleints are the active runs).
+		// - Workers for browsers that have 0 online clients in the swarm.
+		//   (These are workers of which the browser likely crashed or had issues
+		//   joining the swarm).
+
+		for (workerId in percWorkers) {
+			worker = percWorkers[workerId];
+			uaId = self.getUaIdFromWorker(worker);
+			if (!uaId) {
+				if (config.verbose) {
+					// This worker was either created by a different script or by a another version
+					// of this script with different ua map.
+					console.log('Found worker for which there is no match in UA map', worker);
+				}
+				continue;
+			}
+
+			stats = percSwarmStats[uaId];
+
+			if (stats.pendingRuns === 0 && stats.activeRuns === 0) {
+				if (config.verbose) {
+					console.log('No longer needed worker:', {
+						worker: worker,
+						stats: stats
+					});
+				}
+
+				self.terminateWorker(workerId);
+				worker.status = 'terminated';
+
+				// Update perception
+				if (stats.onlineClients > 0) {
+					stats.onlineClients -= 1;
+				}
+				workersByUa[uaId] -= 1;
+
+			// Don't terminate workers stil in the queue that aren't in the swarm yet
+			// TODO: This could terminate a worker that just got started (so status
+			// isn't 'queue' anymore) but hasn't loaded the browser yet. In the future
+			// with event-based testswarm we'll be able to more closely determine this.
+			} else if (worker.status === 'running' && stats.onlineClients === 0) {
+				if (config.verbose) {
+					console.log('Running worker disconnected from the swarm:', {
+						worker: worker,
+						stats: stats
+					});
+				}
+
+				self.terminateWorker(workerId);
+				worker.status = 'terminated';
+
+				// Update perception
+				workersByUa[uaId] -= 1;
+
+			}
+		}
+
+		// Task 2: Start workers for browsers with pending tests but 0 online clients
+		// and 0 workers (this last bit is important as we don't want to spawncr another
+		// worker here if there is one queued but not in the swarm yet).
+		// Note: This is the only case where we ignore the total limit to use the 'queue'
+		// system of browserstack to start all browsers that are needed without question.
+		// It also bypasses priority because it is more important to get at least 1 of
+		// each online so that they can work asynchronous. And then, in Task 2, we'll
+		// fill in extra workers if possible.
+		if (config.verbose) {
+			console.log('\n== Task 2 ==\n'.white.bold);
+		}
+
+		for (ua in percSwarmStats) {
+			stats = percSwarmStats[ua];
+			if (stats.pendingRuns > 0 && stats.onlineClients === 0 && workersByUa[ua] === 0) {
+				self.spawnWorker(browserMap[ua]);
+
+				// Update perception
+				stats.onlineClients += 1;
+				workersByUa[ua] += 1;
+			}
+		}
+
+		// Task 3: Compute the neediness of browsers and spawncr the most
+		// needed browser. Keep doing so until the available slots are filled.
+		if (config.verbose) {
+			console.log('\n== Task 3 ==\n'.white.bold);
+		}
+
+		function workerTotal() {
+			var ua, total = 0;
+			for (ua in workersByUa) {
+				total += workersByUa[ua];
+			}
+			return total;
+		}
+
+		function getNeediest() {
+			var ua, stats, priority, neediest;
+
+			neediest = {
+				priority: 0,
+				ua: undefined
+			};
+
+			for (ua in percSwarmStats) {
+				stats = percSwarmStats[ua];
+				if (workersByUa[ua] >= config.browserstack.eqLimit) {
+					// We've reached the number of max for this ua.
+					priority = 0;
+				} else if (stats.onlineClients === 0) {
+					// We already dealt with this category. If it is still 0,
+					// it means we can't help this one (see Task 1).
+					// Also, x/0 is NaN or Infinity in javascript, which we don't want.
+					priority = 0;
+				} else {
+					// This is the priority formula.
+					// The more runs and the less clients, the higher the priority.
+					// No runs? Priority becomes 0 (0/anything=0).
+					priority = stats.pendingRuns / stats.onlineClients;
+				}
+				if (priority > neediest.priority) {
+					neediest = {
+						priority: priority,
+						ua: ua
+					};
+				}
+			}
+
+			return neediest;
+		}
+
+
+		console.log('Status... (workers: ' + workerTotal() + ' / limit: ' + config.browserstack.totalLimit + ')');
+		while (workerTotal() < config.browserstack.totalLimit) {
+			result = getNeediest();
+			if (result.priority <= 0) {
+				console.log('Neediness exhausted, done!');
+				break;
+			} else {
+				if (config.verbose) {
+					console.log('Most needed:', result);
+				}
+				self.spawnWorker(browserMap[result.ua]);
+
+				// Update perception
+				percSwarmStats[result.ua].onlineClients += 1;
+				workersByUa[result.ua] += 1;
+			}
+			console.log('Looping... (workers: ' + workerTotal() + ' / limit: ' + config.browserstack.totalLimit + ')');
+		}
+
+	},
+
+	run: function () {
+		async.parallel({
+			currentWorkers: function (callback) {
+				self.getBsClient().getWorkers(function (err, resp) {
+					if (err) {
+						console.error('Failed to get list of workers', err);
+					}
+					callback(err, resp);
+				});
+			},
+			swarmState: function (callback) {
+				self.getSwarmState(function (err, state) {
+					if (state) {
+						callback(null, state);
+					} else {
+						console.error('Failed to get testswarm state', err);
+						// TODO handle err, for now just continue pretending there are no needs
+						// by giving it an empty object.
+						callback(null, {
+							userAgents: {}
+						});
+					}
+				});
+			}
+		}, function (err, results) {
+			self.updateBrowsers(results.currentWorkers, results.swarmState);
+		});
+	},
+
+	terminateAll: function () {
+		var client = self.getBsClient();
+		client.getWorkers(function (err, workers) {
+			if (err) {
+				console.error('Could not get workers from browserstack');
+				return;
+			}
+			if (!workers || workers.length < 1) {
+				console.log('No workers running or queued');
+			}
+			workers.forEach(function (worker) {
+				self.terminateWorker(worker);
+			});
+		});
+	},
+
+	getWorker: function (id) {
+		var client = self.getBsClient();
+		client.getWorker(id, function (err, worker) {
+			if (err) {
+				console.error('Could not get worker info from browserstack');
+				return;
+			}
+			if (!worker) {
+				console.log('No worker info available');
+			}
+			console.log('Worker #' + id + ':\n', worker);
+		});
+	}
+};
+
+
+module.exports = self;

lib/testswarm-browserstack.js
@@ -1,261 +1,0 @@
-var request = require('request'),
-    BrowserStack = require('browserstack'),
-    async = require('async'),
-    browserMap = require("./map").map;
-var self;
-
-var TestSwarmBrowserStackInteg = {
-
-    options:function (options) {
-        if (!options) {
-            return self._options;
-        }
-        self._options = options;
-    },
-
-    client:function () {
-        if (self._client) {
-            return self._client;
-        }
-        self._client = BrowserStack.createClient({
-            username:self.options().user,
-            password:self.options().pass
-        });
-        return self._client;
-    },
-
-    /**
-     * Get the swarmstate of testswarm (number of active clients and pending runs).
-     * @param callback Function
-     */
-    getSwarmState:function (callback) {
-        request.get(self.options().swarmUrl + '/api.php?action=swarmstate', function (error, res, body) {
-            var apiData = JSON.parse(body);
-            if (apiData) {
-                if (apiData.error) {
-                    callback(apiData.error);
-                    return;
-                }
-                if (apiData.swarmstate) {
-                    callback(null, apiData.swarmstate);
-                    return;
-                }
-            }
-            callback({
-                code:'unknown',
-                info:'Malformed API response'
-            });
-        });
-    },
-
-    /**
-     * Get a browserstack worker id based on a browserMap object
-     * @param browser Object: Object with property 'name' and 'version' (from testswarm-browserstack.map)
-     * @param currentWorkers Array: List of worker objects (from browserstack.getWorkers)
-     * @return Array: Worker ids (can be empty if none found)
-     */
-    getWorkersByBrowser:function (browser, currentWorkers) {
-        var i, worker,
-            workers = [],
-            len = currentWorkers.length;
-        for (i = 0; i < len; i++) {
-            worker = currentWorkers[i];
-            if (worker.browser.name === browser.name && worker.browser.version === browser.version) {
-                workers.push(worker);
-            }
-        }
-        return workers;
-    },
-
-    startWorker:function (browser, clientTimeout) {
-        if (self.options().dryRun) {
-            console.log('[dryRun] startWorker', browser, clientTimeout);
-            return;
-        }
-        var client = self.client();
-        client.createWorker({
-            browser:browser.name,
-            version:browser.version,
-            url:self.options().swarmRunUrl,
-            timeout:clientTimeout
-        }, function (err, worker) {
-            if (err) {
-                console.log('error spawning browser:', browser, err);
-            } else {
-                console.log('started browser: ', browser, worker);
-            }
-        });
-    },
-
-    /**
-     * @param currentWorkers Array: Array of browser objects
-     * @param swarmState Object: Info about current state of the testswarm
-     */
-    updateBrowsers:function (currentWorkers, swarmState) {
-        var browserID, stats, workers, doKillWorkers, i, len,
-            // Browsers needed by TestSwarm, includes browsers that have workers already
-            neededBrowsers = [],
-            // Browsers to be started. Must not be more than options().stackLimit
-            // May contain the same browser multiple times, this is expected behavior
-            startBrowsers = [],
-            // Workers that are no longer needed
-            killWorkers = [];
-
-        if (self.options().verbose) {
-            console.log('TestSwarm statistics:\n', (function () {
-                // Reduce big swarmState to just the user agent stats
-                var uaID, uaStats = {};
-                for (uaID in swarmState.userAgents) {
-                    uaStats[uaID] = swarmState.userAgents[uaID].stats;
-                }
-                return uaStats;
-            }()));
-            console.log('BrowserStack current workers:\n', currentWorkers);
-        }
-
-        // Figure out which browsers are needed by TestSwarm and which should be killed
-        for (browserID in browserMap) {
-            if (!swarmState.userAgents[browserID]) {
-                continue;
-            }
-            stats = swarmState.userAgents[browserID].stats;
-            workers = self.getWorkersByBrowser(browserMap[browserID], currentWorkers);
-            doKillWorkers = false;
-            if (stats.pendingRuns > 0) {
-                neededBrowsers.push(browserMap[browserID]);
-                if (stats.onlineClients === 0 && workers.length && self.options().kill) {
-                    // There is an active worker but it is not in the swarm. This can
-                    // happen if the browser crashed. Kill the lost worker.
-                    doKillWorkers = true;
-                }
-            } else if (stats.activeRuns === 0 && stats.pendingRuns === 0 && workers.length && self.options().kill) {
-                // Kill workers for browsers for which there are no new runs
-                // available and also are not actively busy running something.
-                // (we don't want to kill a worker that's working on the last run in the list)
-                doKillWorkers = true;
-            }
-
-            if (doKillWorkers) {
-                killWorkers.push.apply(killWorkers, workers);
-            }
-        }
-
-        console.log('TestSwarm wants these browsers:', neededBrowsers);
-        console.log('BrowserStack workers to be killed:', killWorkers);
-
-        // Figure out which of the needed browsers to start.
-        // Summary:
-        // * If the limit is lower than the number of needed browsers, then some
-        // workers won't be started, yet (if we would start them they'd only be
-        // stalled in browserstack's queue).
-        // * When the number of needed browsers has become less than limit, the loop
-        // will be reset one or more times so that more instances of the same browsers
-        // are started (e.g. 2 instances of IE6 and IE7).
-        i = currentWorkers.length - killWorkers.length;
-
-        if (self.options().verbose) {
-               console.log('BrowserStack limit:', self.options().stackLimit);
-               console.log('BrowserStack # workers (minus workers to be killed):', i);
-        }
-
-        for (i = i < 0 ? 0 : i, len = neededBrowsers.length; i < len; i++) {
-            if (startBrowsers.length === self.options().stackLimit) {
-                break;
-            }
-
-            startBrowsers.push(neededBrowsers[i]);
-
-            // Array index starts at zero, -1 is last key.
-            // If this is the last key, reset the loop. We want to keep looping
-            // until we reach the stack limit.
-            if (i === len-1) {
-                // -1 instead of 0. Because after this loop, "i++" will run.
-                i = -1;
-            }
-        }
-        console.log('BrowserStack workers to be started:', startBrowsers);
-
-        killWorkers.forEach(function (worker, i) {
-            self.killWorker(worker);
-        });
-
-        startBrowsers.forEach(function (browser, i) {
-            self.startWorker(browser, self.options().clientTimeout);
-        });
-    },
-
-    run:function () {
-        var client = self.client();
-        async.parallel({
-            currentWorkers:function (callback) {
-                client.getWorkers(function (err, resp) {
-                    if (err) {
-                        console.log('Error getting workers', err);
-                    }
-                    callback(err, resp);
-                });
-            },
-            swarmState:function (callback) {
-                self.getSwarmState(function (error, state) {
-                    if (state) {
-                        callback(null, state);
-                    } else {
-                        console.log('Getting testswarm state failed:\n', error);
-                        // TODO handle err, for now just continue pretending there are no needs
-                        // by giving it an empty object.
-                        callback(null, {
-                            userAgents:{}
-                        });
-                    }
-                });
-            }
-        }, function (err, results) {
-            self.updateBrowsers(results.currentWorkers, results.swarmState);
-        });
-    },
-
-    /**
-     * @param worker Object|Number: Either a worker object (as given by browerstack.getWorkers,
-     * used by tsbs.updateBrowsers), or the worker id directly (used by "cli.js --killWorker")
-     */
-    killWorker:function (worker) {
-        if (self.options().dryRun) {
-            console.log('[dryRun] killWorker', worker);
-            return;
-        }
-        var client = self.client();
-        client.terminateWorker(worker.id || worker, function (err) {
-            if (err) {
-                console.log('could not kill worker', worker);
-                return;
-            }
-            console.log('killed worker', worker);
-        });
-    },
-
-    killAll:function () {
-        var client = self.client();
-        client.getWorkers(function (err, workers) {
-            if (err) {
-                console.log('could not get workers from browserstack');
-                return;
-            }
-            if (!workers || workers.length < 1) {
-                console.log('no workers running or queued');
-            }
-            workers.forEach(function (worker, i) {
-                self.killWorker(worker);
-            });
-        });
-    }
-};
-
-self = TestSwarmBrowserStackInteg;
-
-module.exports = {
-    options:TestSwarmBrowserStackInteg.options,
-    getSwarmState:TestSwarmBrowserStackInteg.getSwarmState,
-    run:TestSwarmBrowserStackInteg.run,
-    killWorker:TestSwarmBrowserStackInteg.killWorker,
-    killAll:TestSwarmBrowserStackInteg.killAll
-};

.gitignore
@@ -1,5 +1,9 @@
-logrotate.conf
+# Using/installing software
 node_modules
+config.json
+logrotate.conf
 run.log
 run.sh
+
+# Other files
 .DS_Store

.jshintignore
@@ -1,0 +1,1 @@
+node_modules

.jshintrc
@@ -1,0 +1,28 @@
+{
+	"bitwise": true,
+	"camelcase": true,
+	"curly": true,
+	"eqeqeq": true,
+	"forin": false,
+	"immed": true,
+	"latedef": true,
+	"newcap": true,
+	"noarg": true,
+	"noempty": true,
+	"nonew": true,
+	"plusplus": false,
+	"quotmark": "single",
+	"regexp": true,
+	"undef": true,
+	"unused": true,
+	"strict": false,
+	"trailing": true,
+
+	"smarttabs": true,
+
+	"node": true,
+
+	"nomen": true,
+	"onevar": true,
+	"white": true
+}

commit 7deaedc0da840ab12a1eafd572810824a46e314c

Fix typo in comment ("Task 3" instead of "Task 2")
Follows-up b626290f which introduced the typo.

Ref #47.

src/testswarm-browserstack.js
@@ -592,7 +592,7 @@ self = {
 		// Note: This is the only case where we ignore the total limit to use the 'queue'
 		// system of browserstack to start all browsers that are needed without question.
 		// It also bypasses priority because it is more important to get at least 1 of
-		// each online so that they can work asynchronous. And then, in Task 2, we'll
+		// each online so that they can work asynchronous. And then, in Task 3, we'll
 		// fill in extra workers if possible.
 		if (config.verbose) {
 			console.log('\n== Task 2 ==\n'.white.bold);